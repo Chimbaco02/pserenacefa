e994d19686e12b17522e8cc3f8f734b65b0d3832
Chimbaco02 <afchimbaco04@gmail.com>
2025-03-19 08:17:59 -0500

    Cambio en Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -159,7 +159,7 @@
 
             <div class="hero-section">
                 <div class="hero-content">
-                    <h1 class="text-4xl font-bold mb-4">Ruben</h1>
+                    <h1 class="text-4xl font-bold mb-4">BIENVENIDOS</h1>
                     <p class="text-xl mb-8">Centro de Formación SENA - Solicita y administra espacios y recursos de manera eficiente</p>
                     @if (Route::has('login'))
                         @auth
